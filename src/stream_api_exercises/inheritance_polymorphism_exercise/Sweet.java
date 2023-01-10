665ebaa2f407f0079c9fbe01573ae227fdf7edf3
rafineria123 <37177277+rafineria123@users.noreply.github.com>
2023-01-10 14:07:26 +0100

    implemented sweets hierarchy classes

---
+++ src/stream_api_exercises/inheritance_polymorphism_exercise/Sweet.java
@@ -0,0 +1,7 @@
+package stream_api_exercises.inheritance_polymorphism_exercise;
+
+public abstract class Sweet {
+    private String name;
+    private double weight;
+    private double sugarWeight;
+}
